e03166bb7386bb75e2369eab49204eb6d17eb13d
Addison Douglas <87285846+AddisonDouglas@users.noreply.github.com>
2023-10-25 17:15:52 -0700

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,2 +1,25 @@
-var element = document.querySelector('div.style-scope ytd-topbar-logo-renderer');
-element.remove(element);
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
+
+  async function deleteStuff(){
+    var element = document.querySelector('div.style-scope ytd-topbar-logo-renderer');
+    element.remove(element);
+    
+    var elements = document.getElementsByClassName("style-scope ytd-rich-grid-media");
+    console.log("hi");
+    for (var i = 0; i < elements.length; i++) {
+        await sleep(1000);
+        var g = elements.item(i);
+        g.remove(g);
+        }
+    
+  }
+
+  deleteStuff()
+
+
+
+
